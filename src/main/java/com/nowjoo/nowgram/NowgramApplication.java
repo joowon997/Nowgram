b98809216c65ebd4ec15d9eec97afc6cb265bfc8
megait <megait@gmail.com>
2024-09-04 21:45:54 +0900

    [정주원 학원] - Nowgram SNS 프로젝트 최초생성 및 환경설정

---
+++ src/main/java/com/nowjoo/nowgram/NowgramApplication.java
@@ -0,0 +1,13 @@
+package com.nowjoo.nowgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowgramApplication.class, args);
+	}
+
+}
